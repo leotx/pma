6423e413c1a1f0b54e052e64c610ab6e68e8f8d8
Leo <leonardo.xavier@dextra-sw.com>
2015-06-18 22:31:18 -0300

    Button Appointment

--- PMA/Resources/Resource.Designer.cs
+++ PMA/Resources/Resource.Designer.cs
@@ -64,31 +64,34 @@ namespace PMA
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int button_default_bg = 2130837504;
+			public const int button_appointment = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int edittext_bottom_bg = 2130837505;
+			public const int button_default_bg = 2130837505;
 			
 			// aapt resource value: 0x7f020002
-			public const int edittext_default_bg = 2130837506;
+			public const int edittext_bottom_bg = 2130837506;
 			
 			// aapt resource value: 0x7f020003
-			public const int edittext_top_bg = 2130837507;
+			public const int edittext_default_bg = 2130837507;
 			
 			// aapt resource value: 0x7f020004
-			public const int email = 2130837508;
+			public const int edittext_top_bg = 2130837508;
 			
 			// aapt resource value: 0x7f020005
-			public const int Icon = 2130837509;
+			public const int email = 2130837509;
 			
 			// aapt resource value: 0x7f020006
-			public const int monoandroidsplash = 2130837510;
+			public const int Icon = 2130837510;
 			
 			// aapt resource value: 0x7f020007
-			public const int password = 2130837511;
+			public const int monoandroidsplash = 2130837511;
 			
 			// aapt resource value: 0x7f020008
-			public const int user = 2130837512;
+			public const int password = 2130837512;
+			
+			// aapt resource value: 0x7f020009
+			public const int user = 2130837513;
 			
 			static Drawable()
 			{
@@ -126,7 +129,10 @@ namespace PMA
 		{
 			
 			// aapt resource value: 0x7f030000
-			public const int Main = 2130903040;
+			public const int Appointment = 2130903040;
+			
+			// aapt resource value: 0x7f030001
+			public const int Main = 2130903041;
 			
 			static Layout()
 			{
